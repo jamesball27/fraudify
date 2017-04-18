43e8061402c6b8beb5549d41f0a17cbd3b53e840
jamesball27 <jwrb27@gmail.com>
2017-04-18 17:32:21 -0400

    Bootstrap currentUser

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -4,7 +4,15 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
+  
   window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
