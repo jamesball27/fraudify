0a6af700ba29352c88106d33ee406b4f5e8cbd9e
jamesball27 <jwrb27@gmail.com>
2017-04-23 11:14:30 -0400

    Add artists to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,23 @@ guest = User.create(username: "guest", password: "password")
 users = User.all
 
 Artist.destroy_all
-test_artist1 = Artist.create(name: "TestArtist1", image: nil)
-test_artist2 = Artist.create(name: "TestArtist2", image: nil)
+bach = Artist.create(name: "Johann Sebastian Bach", image: "https://s3.amazonaws.com/fraudify-dev/artist-images/bach.jpg")
+beethoven = Artist.create(name: "Ludwig van Beethoven", image: "https://s3.amazonaws.com/fraudify-dev/artist-images/beethoven.jpg")
+brahms = Artist.create(name: "Johannes Brahms", image: "https://s3.amazonaws.com/fraudify-dev/artist-images/brahms.jpg")
+haydn = Artist.create(name: "Franz Joseph Haydn", image: "https://s3.amazonaws.com/fraudify-dev/artist-images/haydn.jpg")
+holst = Artist.create(name: "Gustav Holst", image: "https://s3.amazonaws.com/fraudify-dev/artist-images/holst.jpg")
+mahler = Artist.create(name: "Gustav Mahler", image: "https://s3.amazonaws.com/fraudify-dev/artist-images/mahler.jpg")
+mozart = Artist.create(name: "Wolfgang Amadeus Mozart", image: "https://s3.amazonaws.com/fraudify-dev/artist-images/mozart.jpg")
+ravel = Artist.create(name: "Maurice Ravel", image: "https://s3.amazonaws.com/fraudify-dev/artist-images/ravel.jpg")
+tchaikovksy = Artist.create(name: "Pyotr Ilyich Tchaikovsky", image: "https://s3.amazonaws.com/fraudify-dev/artist-images/tchaikovsky.jpg")
+vivaldi = Artist.create(name: "Antonio Vivaldi", image: "https://s3.amazonaws.com/fraudify-dev/artist-images/vivaldi.jpg")
 
 Album.destroy_all
 test_album1 = Album.create(title: "Greatest Hits", artist_id: test_artist1.id)
 test_album2 = Album.create(title: "Greatest Hits pt. 2", artist_id: test_artist2.id)
 
 Song.destroy_all
-Song.create(title: "song1", artist_id: test_artist1.id, album_id: test_album1.id, duration: 300, album_ord: 1)
+song1 = Song.create(title: "song1", artist_id: bach.id, album_id: test_album1.id, duration: 300, album_ord: 1)
 Song.create(title: "song2", artist_id: test_artist1.id, album_id: test_album1.id, duration: 300, album_ord: 2)
 Song.create(title: "song3", artist_id: test_artist1.id, album_id: test_album1.id, duration: 300, album_ord: 3)
 Song.create(title: "song4", artist_id: test_artist1.id, album_id: test_album1.id, duration: 300, album_ord: 4)
@@ -45,7 +53,7 @@ playlist2 = Playlist.create(name: "playlist2", creator_id: guest.id)
 
 
 PlaylistSong.destroy_all
-PlaylistSong.create(playlist_id: playlist1.id, song_id: songs.sample.id, ord: 1)
+PlaylistSong.create(playlist_id: playlist1.id, song_id: song1.id, ord: 1)
 PlaylistSong.create(playlist_id: playlist1.id, song_id: songs.sample.id, ord: 2)
 PlaylistSong.create(playlist_id: playlist1.id, song_id: songs.sample.id, ord: 3)
 PlaylistSong.create(playlist_id: playlist1.id, song_id: songs.sample.id, ord: 4)
