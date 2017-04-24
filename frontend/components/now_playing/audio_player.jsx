10702afe24bb8278887f1266072604df5ae92acc
jamesball27 <jwrb27@gmail.com>
2017-04-24 15:14:39 -0400

    Add ability to scroll through song

--- frontend/components/now_playing/audio_player.jsx
+++ frontend/components/now_playing/audio_player.jsx
@@ -4,29 +4,57 @@ class AudioPlayer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { audioUrl: '', playing: false };
+    this.state = { audioUrl: '', playing: false, duration: null, elapsed: 0 };
 
-    this.play = this.play.bind(this);
+    this.togglePlay = this.togglePlay.bind(this);
+    this.updateTimeline = this.updateTimeline.bind(this);
+    this.movePlayhead = this.movePlayhead.bind(this);
+    this.scrollPlayback = this.scrollPlayback.bind(this);
   }
 
   componentWillReceiveProps(newProps) {
+    this.audio = document.getElementById('audio');
     if (this.state.audioUrl !== newProps.currentSong.audioUrl) {
       this.setState({ audioUrl: newProps.currentSong.audioUrl });
     }
   }
 
-  play() {
-
-    const audio = document.getElementById('audio');
+  togglePlay() {
     if (!this.state.playing) {
-      audio.play();
-      this.setState({ playing: true });
-
+      this.audio.play();
+      this.updateTimeline();
+      this.setState({ playing: true, duration: this.audio.duration });
     } else {
-      audio.pause();
+      this.audio.pause();
+      window.clearInterval(this.updateInterval);
       this.setState({ playing: false });
-
     }
+  }
+
+  updateTimeline() {
+    this.updateInterval = window.setInterval(() => {
+      this.setState({ elapsed: this.audio.currentTime });
+      this.movePlayhead();
+    }, 1000);
+  }
+
+  movePlayhead() {
+    const playhead = document.getElementById('playhead');
+    const playPercent = (this.state.elapsed / this.state.duration) * 100;
+    playhead.style.marginLeft = playPercent + '%';
+  }
+
+  scrollPlayback(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const timeline = document.getElementById('timeline');
+    const width = timeline.getBoundingClientRect().width;
+    const left = timeline.getBoundingClientRect().left;
+    const percent = (e.clientX - left) / width;
+
+    this.audio.currentTime = this.state.duration * percent;
+    this.setState({ elapsed: this.audio.currentTime });
   }
 
   render() {
@@ -35,12 +63,14 @@ class AudioPlayer extends React.Component {
     return(
       <div className="audio-player">
 
-        <audio id="audio" src={ this.state.audioUrl }>
+        <audio id="audio" src={ this.state.audioUrl }></audio>
 
-        </audio>
+        <img src={ pButtonUrl } className="p-button" onClick={ this.togglePlay } />
 
-        <img src={ pButtonUrl } className="p-button" onClick={ this.play } />
-
+        
+        <div id="timeline" onClick={ this.scrollPlayback }>
+          <div id="playhead"></div>
+        </div>
       </div>
     );
   }
