1bd9f2db7ac5260e98bab021e9348ee4540ea0f6
jamesball27 <jwrb27@gmail.com>
2017-05-09 15:46:08 -0400

    Add webpack config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './frontend/entry.jsx',
@@ -21,5 +22,17 @@ module.exports = {
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*']
-  }
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ]
 };
